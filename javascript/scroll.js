16eb91ac71d3b3898c07259a0a7ed84baad07be1
GaoFei <strivek@163.com>
2015-01-21 19:04:27 +0800

    增加无限加载基础代码

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,30 +1,44 @@
-function getViewPortH() {
-        return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    }
+function getViewportH() {
+    return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
 
-    function scrollY() {
-        return window.pageYOffset || document.scrollTop;
-    }
+function scrollY() {
+    return window.pageYOffset || document.scrollTop;
+}
 
-    //http://stackoverflow.com/a/5598797/989439
-    function getOffset(el) {
-        var offsetTop = 0,
-            offsetLeft = 0;
-        do {
-            if (!isNaN(el.offsetTop)) {
-                offsetTop += el.offsetTop;
-                console.log(offsetTop);
-            }
-            if (!isNaN(el.offsetLeft)) {
-                offsetLeft += el.offsetLeft;
-            }
-        } while (el = el.offsetParent)
-
-        return {
-            top: offsetTop,
-            left: offsetLeft
+//http://stackoverflow.com/a/5598797/989439
+function getOffset(el) {
+    var offsetTop = 0,
+        offsetLeft = 0;
+    do {
+        if (!isNaN(el.offsetTop)) {
+            offsetTop += el.offsetTop;
         }
+        if (!isNaN(el.offsetLeft)) {
+            offsetLeft += el.offsetLeft;
+        }
+    } while (el = el.offsetParent)
+
+    return {
+        top: offsetTop,
+        left: offsetLeft
     }
-    var elem = document.querySelector("h1");
-    
-    getOffset(elem);
+}
+
+function getDocumentHeight() {
+    return window.document.documentElement.offsetHeight;
+}
+
+function inViewport(el, h) {
+    var docH = getDocumentHeight(),
+        elH = el.offsetHeight,
+        viewPort = getViewportH(),
+        scrolled = scrollY(),
+        elTop = getOffset(el).top,
+        windowBottomToBottom = 1 - (docH - scrolled - viewPort) / elH;
+    return windowBottomToBottom;
+}
+var elem = document.querySelector(".list2");
+window.addEventListener('scroll', function() {
+    console.log(inViewport(elem));
+})
